beb97235d7343cf9e054690c601fae92524fea17
Bernard <b.mullerjnr@gmail.com>
2022-09-28 11:36:51 +0200

    stuff

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0943c2fcede5ec9a"; }
+/******/ 		__webpack_require__.h = function() { return "1b676ce36902f111"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
