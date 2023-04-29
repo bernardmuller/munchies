a188d97de69d42f2a420309a0dfeed841cc8109e
Bernard <b.mullerjnr@gmail.com>
2023-04-29 09:31:29 +0200

    refactor: new login button

--- mobile/src/navigation/stacks/SettingsStack.tsx
+++ mobile/src/navigation/stacks/SettingsStack.tsx
@@ -1,15 +1,47 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as React from "react";
 import SettingsScreen from "../../screens/settings";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigation } from "@react-navigation/native";
+import { DeleteIcon, IconButton } from "native-base";
+import { FiLogOut } from "react-icons/fi";
+import { TouchableOpacity } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SettingsStack = createNativeStackNavigator();
 
 function Settings() {
-  return (
-    <SettingsStack.Navigator>
-      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
-    </SettingsStack.Navigator>
-  );
+	const { clearToken } = React.useContext(AuthContext);
+	const navigation = useNavigation();
+
+	const handleLogout = () => {
+		clearToken();
+		navigation.navigate("Login");
+	};
+
+	return (
+		<SettingsStack.Navigator>
+			<SettingsStack.Screen
+				name="Settings"
+				component={SettingsScreen}
+				options={{
+					title: "",
+					headerRight: () => (
+						<>
+							<TouchableOpacity onPress={() => handleLogout()}>
+								{/* <FiLogOut size={18} /> */}
+								<Feather
+									name="log-out"
+									size={24}
+									color="black"
+								/>
+							</TouchableOpacity>
+						</>
+					),
+				}}
+			/>
+		</SettingsStack.Navigator>
+	);
 }
 
 export default Settings;
--- mobile/src/screens/settings/index.tsx
+++ mobile/src/screens/settings/index.tsx
@@ -3,20 +3,8 @@ import * as React from "react";
 import { View } from "../../components/common/View";
 import { AuthContext } from "../../contexts/AuthContext";
 
-function Recipes({ navigation }: { navigation: any }) {
-  const { clearToken } = React.useContext(AuthContext);
-
-  const handleLogout = () => {
-    clearToken();
-    navigation.navigate("Login")
-  }
-
-  return (
-    <View className="grid gap-1 mt-1">
-      <Button title="Log out" onPress={handleLogout}
-      />
-    </View>
-  );
+function Settings({ navigation }: { navigation: any }) {
+	return <View className="grid gap-1 mt-1"></View>;
 }
 
-export default Recipes;
+export default Settings;
