99d96753c48df6db1cdeaddecf4f9dd651cf8021
bernardmuller <b.mullerjnr@gmail.com>
2024-06-27 23:18:21 +0200

    feat: add new mobile app init

---
+++ mobile/app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Text>Edit app/index.tsx to edit this screen.</Text>
+		</View>
+	);
+}
