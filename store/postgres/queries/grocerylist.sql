43dc097dd778252c4e9abca658848d5f1ff5873b
Bernard Muller <b.mullerjnr@gmail.com>
2024-09-11 09:02:35 +0200

    feat: aggregated grocerylist sql queries

--- store/postgres/queries/grocerylist.sql
+++ store/postgres/queries/grocerylist.sql
@@ -7,3 +7,71 @@ SELECT * FROM grocerylists ORDER BY createdat DESC LIMIT 1;
 -- name: CreateGrocerylist :one
 INSERT INTO grocerylists (id, menu_id, household_id) VALUES ($1, $2, $3) RETURNING *;
 
+-- name: GetGrocerylistWithItemsById :one
+SELECT
+  gl.id AS grocerylist_id,
+  gl.household_id,
+  gl.menu_id,
+  JSON_AGG(
+    JSON_BUILD_OBJECT(
+      'item_id', i.id,
+      'check', i.check,
+      'name', ing.name 
+    )
+  ) AS items
+FROM
+  grocerylists gl
+LEFT JOIN
+  items i ON i.grocerylist_id = gl.id
+LEFT JOIN
+  ingredients ing ON ing.id = i.ingredient_id
+WHERE
+  gl.id = $1
+GROUP BY
+  gl.id;
+
+-- name: GetGrocerylistWithItemsByUserId :one
+SELECT
+  gl.id AS grocerylist_id,
+  gl.household_id,
+  gl.menu_id,
+  JSON_AGG(
+    JSON_BUILD_OBJECT(
+      'item_id', i.id,
+      'check', i.check,
+      'name', ing.name 
+    )
+  ) AS items
+FROM
+  grocerylists gl
+LEFT JOIN
+  items i ON i.grocerylist_id = gl.id
+LEFT JOIN
+  ingredients ing ON ing.id = i.ingredient_id
+WHERE
+  gl.createdby = $1
+GROUP BY
+  gl.id;
+
+-- name: GetGrocerylistWithItemsByHouseholdId :one
+SELECT
+  gl.id AS grocerylist_id,
+  gl.household_id,
+  gl.menu_id,
+  JSON_AGG(
+    JSON_BUILD_OBJECT(
+      'item_id', i.id,
+      'check', i.check,
+      'name', ing.name 
+    )
+  ) AS items
+FROM
+  grocerylists gl
+LEFT JOIN
+  items i ON i.grocerylist_id = gl.id
+LEFT JOIN
+  ingredients ing ON ing.id = i.ingredient_id
+WHERE
+  gl.household_id = $1
+GROUP BY
+  gl.id;
