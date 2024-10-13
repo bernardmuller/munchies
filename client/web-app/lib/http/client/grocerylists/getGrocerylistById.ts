b782facf1136caaa46720e97991aa0014da0e9fe
Bernard Muller <b.mullerjnr@gmail.com>
2024-10-13 21:51:32 +0200

    chore: clean up

--- client/web-app/lib/http/client/grocerylists/getGrocerylistById.ts
+++ client/web-app/lib/http/client/grocerylists/getGrocerylistById.ts
@@ -1,7 +1,6 @@
 import apiRoutes from "../../routes";
 import { httpRequest } from "../../httpRequest";
-import { z } from 'zod';
-import {GroceryList} from "@/lib/http/client/grocerylists/getLatestGrocerylistByUserId";
+import { GroceryList } from "@/lib/http/client/grocerylists/getLatestGrocerylistByUserId";
 
 export async function getGrocerylistById(token: string, id: string) {
   return await httpRequest<GroceryList, void>(
