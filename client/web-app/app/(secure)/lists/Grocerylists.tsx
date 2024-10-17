87d6065bef6620aab14788b701bc23d0c77ea0e8
Bernard Muller <b.mullerjnr@gmail.com>
2024-10-17 15:01:38 +0200

    fix: infinite loading

--- client/web-app/app/(secure)/lists/Grocerylists.tsx
+++ client/web-app/app/(secure)/lists/Grocerylists.tsx
@@ -445,8 +445,7 @@ export default function GroceryListPage({
     checkOrUncheckHouseholdItemsWithSameName(id)
   }
 
-  if ((isMyGrocerylistLoading || isMyHouseholdGrocerylistLoading) ||
-    (!myGrocerylist.items?.length || !myHouseholdGrocerylist?.items?.length)
+  if ((isMyGrocerylistLoading || isMyHouseholdGrocerylistLoading)
   ) {
     return <div className="container mx-auto p-4 space-y-4">
       <div className="flex justify-between items-center">
